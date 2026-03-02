d53b39d6145625e78f16711003aafeccf115213f
Bane <taxi.011@gmail.com>
2026-03-02 15:39:29 +0100

    stigao do ovde

--- core/settings.py
+++ core/settings.py
@@ -35,25 +35,29 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-"django.contrib.admin",
-"django.contrib.auth",
-"django.contrib.contenttypes",
-"django.contrib.sessions",
-"django.contrib.messages",
-"django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
 
 # Third party
-"rest_framework",
+    "rest_framework",
 
 # Local apps
-"authentication",
-"prompts",
-"engine",
-"usage",
+    "authentication",
+    "prompts",
+    "engine",
+    "usage",
 
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,13 +91,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-"default": {
-"ENGINE": "django.db.backends.postgresql",
-"NAME": os.getenv("DB_NAME"),
-"USER": os.getenv("DB_USER"),
-"PASSWORD": os.getenv("DB_PASSWORD"),
-"HOST": os.getenv("DB_HOST"),
-"PORT": os.getenv("DB_PORT"),
+    "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.getenv("DB_NAME"),
+    "USER": os.getenv("DB_USER"),
+    "PASSWORD": os.getenv("DB_PASSWORD"),
+    "HOST": os.getenv("DB_HOST"),
+    "PORT": os.getenv("DB_PORT"),
 }
 }
 
@@ -165,4 +169,9 @@ INSTALLED_APPS = [
     "prompts",
     "engine",
     "usage",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
